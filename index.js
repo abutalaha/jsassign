33aa856dc6740bbf994dd0396e21107ff424fccc
abutalaha <abutalaha6@gmail.com>
2021-01-13 17:25:53 +0530

    jsassign

---
+++ index.js
@@ -0,0 +1,64 @@
+var a=78
+var b=2.5
+var c='work on java script'
+console.log(a);
+console.log(c);
+
+var j=true;
+var l=false;
+
+console.log(typeof(c));
+console.log(typeof(j));
+console.log(typeof(l));
+console.log(typeof(g));
+// console.log(tyepof(=));
+var g=1.52
+
+
+// if else condition 
+var a=15;
+if(a==20){
+console.log('this is true')
+}else {
+    console.log('this is false')
+}
+
+// using logical opersters and or not 
+var a=22;
+var b=-66;
+if(a>=0 && b==-66){
+    console.log('true')
+} 
+else {('false')
+}
+var m=5
+var x=-9
+if (m<=9 || x>=-10){
+    console.log('correct statement')
+}else{
+    console.log('wrong statement')
+}
+
+// // looping statements
+// for loop 
+// syntax for (initilization,condition,incri/decrement)
+for (var a=1; a<=10;a++){
+    console.log(a)
+}
+
+function thank(name){
+    console.log('welcome'+ name)
+}
+ thank('talha')
+ thank('abu')
+ thank('khan')
+
+//  RETURN 
+function add (a,b) {
+    var c=a+b ;
+    return c;
+}
+var res=add(6, 4);
+console.log(res);
+
+
